912c42018772d33d746d8b00892b692534aed759
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-10 15:31:00 -0500

    Improving organization of styles between dashboard and index

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,12 +1,7 @@
 var args = arguments[0] || {};
 
 function goToPayBill (event) {
-	var payBillView = Alloy.createController("paybill", {}).getView();
+	
+	Alloy.Globals.Navigator.open("paybill", {});
 
-    if (OS_IOS) {
-        $.nav.openWindow(payBillView);
-    }
-    if (OS_ANDROID) {
-        payBillView.open();
-    }
 }
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,24 @@
+/**
+ * Global Navigation Handler
+ */
+Alloy.Globals.Navigator = {
+
+	open: function(controller, payload){
+		var win = Alloy.createController(controller, payload || {}).getView();
+
+		if(OS_IOS){
+			$.nav.openWindow(win);
+		}
+		else if(OS_MOBILEWEB){
+			$.nav.open(win);
+		}
+		else {
+			win.open();
+		}
+	}
+};
+
+
 
 if(OS_IOS){
 	$.nav.open()
