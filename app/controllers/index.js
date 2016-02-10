7ae36384a98a644714554493ceb2abf274663430
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-10 14:20:25 -0500

    Navigate to pay bill page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,12 @@
+function goToPayBill (event) {
+	var payBillView = Alloy.createController("paybill", {}).getView();
+
+    if (OS_IOS) {
+        $.index.openWindow(payBillView);
+    }
+    if (OS_ANDROID) {
+        payBillView.open();
+    }
+}
+
 $.index.open();
