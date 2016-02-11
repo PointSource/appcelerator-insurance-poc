3c6aa5cb62cbf77905a510cdfbf2a09b17fef368
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-11 15:47:35 -0500

    Using camera widget to combine iOS and Android functionality

--- app/controllers/paybill.js
+++ app/controllers/paybill.js
@@ -1,78 +1,16 @@
-var args = arguments[0] || {};
-var camera;
+function onPictureTaken(event) {
+    console.log(event.image); // the taken photo
 
-if (OS_ANDROID) {
-
-	$.payBillWindow.orientationModes = [Ti.UI.LANDSCAPE];
-
-	if( Ti.Media.isCameraSupported ) {
-	    var androidcamera = require("pw.custom.androidcamera");
-	    camera = androidcamera.createCameraView({
-	        save_location: "my_app",
-	        pictureTimeout: 0,
-	        resolutionNamed: androidcamera.RESOLUTION_480
-	    });
-
-	    camera.addEventListener("picture_taken", function(event){
-	        alert("Image saved to "+event.path);
-	    });
-
-	    $.cameraView.addEventListener("close", function(){
-	        camera = null;
-	    });
-
-	    $.cameraView.add(camera);
-	} else {
-	    alert("No camera found!");
-	}
-
+    var imageView = Ti.UI.createImageView({
+		width:70,
+		image:event.image
+	});
+	$.imagePreview.add(imageView);
 }
 
-
+function switchCamera() {
+	$.camera.switchCamera();
+}
 function takePicture() {
-
-	if(OS_IOS){
-		Titanium.Media.showCamera({
-			success:function(event) {
-				// called when media returned from the camera
-				Ti.API.debug('Our type was: '+event.mediaType);
-				if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
-					var imageView = Ti.UI.createImageView({
-						width:win.width,
-						height:win.height,
-						image:event.media
-					});
-					win.add(imageView);
-				} else {
-					alert("got the wrong type back ="+event.mediaType);
-				}
-			},
-			cancel:function() {
-				// called when user cancels taking a picture
-			},
-			error:function(error) {
-				// called when there's an error
-				var a = Titanium.UI.createAlertDialog({title:'Camera'});
-				if (error.code == Titanium.Media.NO_CAMERA) {
-					a.setMessage('Please run this test on device');
-				} else {
-					a.setMessage('Unexpected error: ' + error.code);
-				}
-				a.show();
-			},
-			saveToPhotoGallery:true,
-		    // allowEditing and mediaTypes are iOS-only settings
-			allowEditing:true,
-			mediaTypes:[Ti.Media.MEDIA_TYPE_VIDEO,Ti.Media.MEDIA_TYPE_PHOTO]
-		});
-	}
-
-	else if (OS_ANDROID) {
-		if( Ti.Media.isCameraSupported ) {
-	        camera.snapPicture();
-	    }	else {
-	    	alert("No camera found!");
-		}
-
-	}
+	$.camera.snapPicture();
 }
