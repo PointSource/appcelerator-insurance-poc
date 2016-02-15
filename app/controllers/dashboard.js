a8230f9ebdf8b7d6402711bddeaef864cb71e56d
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-15 12:46:52 -0500

    Refactoring menu into index instead of dashboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,40 +1,6 @@
 var args = arguments[0] || {};
-var menuOpen = false;
 
 function goToPayBill (event) {
 	Alloy.Globals.Navigator.open("paybill", {});
 }
 
-function openMenu() {
-    $.SlideMenu.animate({
-        right: "0dp",
-        duration: 250,
-        curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-    });
-    menuOpen = true;
-}
-
-function closeMenu() {
-	$.SlideMenu.animate({
-	    right: "-200dp",
-	    duration: 250,
-	    curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-	});
-	menuOpen = false;
-}
-
-function toggleMenu() {
-	if (!menuOpen) {
-	    openMenu();
-	} else {
-		closeMenu();
-	}
-}
-
-$.AppWrapper.addEventListener("swipe", function(_event) {
-    if(_event.direction == "left") {
-        openMenu();
-    } else if(_event.direction == "right") {
-        closeMenu();
-    }
-});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,5 @@
+var menuOpen = false;
+
 /**
  * Global Navigation Handler
  */
@@ -19,10 +21,44 @@ Alloy.Globals.Navigator = {
 };
 
 
+function openMenu() {
+    $.SlideMenu.animate({
+        right: "0dp",
+        duration: 250,
+        curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+    });
+    menuOpen = true;
+}
+
+function closeMenu() {
+	$.SlideMenu.animate({
+	    right: "-200dp",
+	    duration: 250,
+	    curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+	});
+	menuOpen = false;
+}
+
+function toggleMenu() {
+	if (!menuOpen) {
+	    openMenu();
+	} else {
+		closeMenu();
+	}
+}
+
+$.AppWrapper.addEventListener("swipe", function(_event) {
+    if(_event.direction == "left") {
+        openMenu();
+    } else if(_event.direction == "right") {
+        closeMenu();
+    }
+});
+
 
 if(OS_IOS){
 	$.nav.open()
 }
 else{
-	$.index.getView().open();
+	$.index.open();
 }
