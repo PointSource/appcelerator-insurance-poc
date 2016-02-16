d644239f718fd0ea0d52e44c36cf93ff4d83c520
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-16 15:08:50 -0500

    Added in module for customizing action bar. Now we have the Point Insurance logo and the menu icon in the Android header. (Point Insurance logo needs to be resized)

---
+++ app/assets/lib/icomoon.js
@@ -0,0 +1,6 @@
+exports.fontfamily = 'icomoon';
+
+
+exports.charcode = {
+	'menu': 0xE90E
+};
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,10 @@
+var abx = require('com.alcoapps.actionbarextras');
+var IconicFont = require('/lib/IconicFont');
+var icomoon = new IconicFont({
+    font: '/lib/icomoon'
+});
+
+
 /**
  * Global Navigation Handler
  */
@@ -42,5 +49,47 @@ if(OS_IOS){
 	$.nav.open()
 }
 else{
+	$.index.addEventListener("open", function() {
+
+	    var activity = $.index.getActivity();
+		
+	    if(activity){
+
+	    	// Add menu items
+			activity.onCreateOptionsMenu = function(e){
+
+				// first, create the item...
+				var settingsItem = e.menu.add({
+					itemId: 101, // don't forget to set an id here
+					title: "Settings",
+					showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS
+				});
+
+				settingsItem.addEventListener('click', function(){
+					toggleMenu();
+				});
+
+				// ...then, let abx apply the custom font
+				abx.setMenuItemIcon({
+					menu: e.menu,
+					menuItem: settingsItem,
+					fontFamily: icomoon.fontfamily,
+					icon: icomoon.icon("menu"),
+					color: "#49a7f7",
+					size: 30
+				});
+			}
+
+
+			abx.title = "Point Insurance";
+            abx.setDisplayShowTitleEnabled( false )
+
+            activity.actionBar.displayHomeAsUp = true;
+		    abx.setHomeAsUpIcon("/images/point-insurance-logo.png");
+		}
+	});
+
 	$.index.open();
+
+
 }
