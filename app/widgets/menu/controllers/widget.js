04fe044838793a23facd5cb90f78cc002ff68c4c
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-15 20:21:04 -0500

    Adding content to side menu

--- app/widgets/menu/controllers/widget.js
+++ app/widgets/menu/controllers/widget.js
@@ -1,5 +1,7 @@
 var menuOpen = false;
 
+$.agentPhoto.image = WPATH('images/Point-Insurance-Agent-Photo-menu.png');
+
 function openMenu() {
     $.SlideMenu.animate({
         right: "0dp",
@@ -11,7 +13,7 @@ function openMenu() {
 
 function closeMenu() {
 	$.SlideMenu.animate({
-	    right: "-200dp",
+	    right: "-280dp",
 	    duration: 250,
 	    curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
 	});
