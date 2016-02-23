0e93ace93e52dccc7b77ffdb05ca6751f5926aab
Patrick Wolf <patrick.wolf@pointsourcellc.com>
2016-02-23 10:20:24 -0500

    Fix listeners getting registered too many times on card scan

--- app/controllers/paybill.js
+++ app/controllers/paybill.js
@@ -1,22 +1,34 @@
 var cardio = require('com.pointsource.card.io');
+
+function handleScan(e) {
+	console.log('scan event', e);
+	var cardNumber = e.cardNumber;
+	var cardType = e.cardType;
+	var cardholderName = e.cardholderName;
+	var cvv = e.cvv;
+	var expiryMonth = e.expiryMonth;
+	var expiryYear = e.expiryYear;
+	var postalCode = e.postalCode;
+	removeListeners();
+}
+
+function handleCancel(e) {
+	console.log("Canceled scan");
+	removeListeners();
+}
+
+function removeListeners() {
+	cardio.removeEventListener("scan", handleScan);
+	cardio.removeEventListener("cancel", handleCancel);
+}
+
+
 function openCardIO() {
 
+	cardio.addEventListener("scan", handleScan);
+	cardio.addEventListener("cancel", handleCancel);
+
 	console.log("Opening Card.io");
-	cardio.addEventListener("scan", function(e) {
-		console.log('scan event', e);
-		var cardNumber = e.cardNumber;
-		var cardType = e.cardType;
-		var cardholderName = e.cardholderName;
-		var cvv = e.cvv;
-		var expiryMonth = e.expiryMonth;
-		var expiryYear = e.expiryYear;
-		var postalCode = e.postalCode;
-	});
-
-	cardio.addEventListener("cancel", function(err, res) {
-		console.log("Canceled scan");
-	});
-
 	cardio.scanCard({
 		languageOrLocale: "fr",
 		collectPostalCode: true,
