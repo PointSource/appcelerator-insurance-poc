873bb5a58cbde09700918faa082d94873cca728a
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-03-03 09:38:48 -0500

    Changing color of minimum due label according to due date

--- app/controllers/payBill/paymentOptions.js
+++ app/controllers/payBill/paymentOptions.js
@@ -15,6 +15,24 @@ function init() {
 	$.currentPolicy.set(args.currentPolicy.attributes);
 
 	$.paymentOptions.title = "PAY "+args.currentPolicy.get("type");
+
+	switch (args.currentPolicy.get("type")) {
+		case "AUTO":
+			$.typeIcon.text = Alloy.Globals.icomoon.icon("main-auto");
+			break;
+		case "RENTERS":
+		case "HOME":
+			$.typeIcon.text = Alloy.Globals.icomoon.icon("main-home");
+			break;
+		default:
+			$.typeIcon.text = Alloy.Globals.icomoon.icon("main-auto");
+	}
+
+	if (args.currentPolicy.getIsOverdue()) {
+		$.minimumDueTitle.color = Alloy.Globals.Colors.primary_accent;
+		$.minimumDueTitle.text = "MINIMUM DUE NOW";
+		$.minimumDueAmount.color =  Alloy.Globals.Colors.primary_accent;
+	}
 }
 
 init();
