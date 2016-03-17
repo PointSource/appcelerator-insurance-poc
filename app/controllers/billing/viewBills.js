13b158ff7690a2d0f91302bd1af67ad378cd0dae
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-03-17 14:19:38 -0400

    Working on card layout on Android

--- app/controllers/billing/viewBills.js
+++ app/controllers/billing/viewBills.js
@@ -5,18 +5,25 @@ var controller = {
 }
 
 function formatPolicy(policy) {
-	Alloy.Globals.apm.leaveBreadcrumb("enter formatPolicy: billList");
 	var transformed = policy.toJSON();
+	var lastPolicyNumber = Alloy.Collections.policy.last()
+							.get("policyNumber");
+	var dueDate = policy.getDueDate();
+	transformed.bottom = 0;
+
+	// Add padding to the last element in the list
+	if (policy.get("policyNumber") === lastPolicyNumber) {
+		transformed.bottom = 15;
+		transformed.backgroundColor = "red";
+	}
 
 	transformed.minimumDue = string.formatCurrency(policy.getMinimumDue());
-	var dueDate = policy.getDueDate();
 	transformed.formattedDueDate = dueDate.format("MMMM DD, YYYY").toUpperCase();
 
 	transformed.getFormattedDriver = policy.getFormattedDriver;
 	transformed.getFormattedVehicle = policy.getFormattedVehicle;
 	transformed.typeIcon = policy.getIcon();
 
-	Alloy.Globals.apm.leaveBreadcrumb("exit formatPolicy: billList");
 	return transformed;
 }
 
@@ -36,22 +43,22 @@ function filterPolicies (policyCollection) {
 function init() {
 	Ti.Analytics.featureEvent(Ti.Platform.osname+"."+controller.title+".viewed");
 
-	Alloy.Globals.apm.leaveBreadcrumb("enter init: billList");
+	Alloy.Globals.apm.leaveBreadcrumb("enter init: "+controller.title);
     Alloy.Globals.setUpNavBar({
-        currentWindow: $.billList,
+        currentWindow: $.viewBills,
         appWrapper: $.AppWrapper
     });
 
     Alloy.Collections.policy.fetch({
     	success: function () {
-
     	},
     	error: function (err) {
     		alert('could not get list of policies');
 		    Alloy.Globals.apm.logHandledException(err);
     	}
     });
-	Alloy.Globals.apm.leaveBreadcrumb("exit init: billList");
+	Alloy.Globals.apm.leaveBreadcrumb("exit init: "+controller.title);
+
 }
 
 init();
